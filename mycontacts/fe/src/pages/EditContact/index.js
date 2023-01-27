0697c59c96a9b0aba99f637d6d7d89d4e7c0d682
Alessandre Livramento <aleliv74@gmail.com>
2023-01-27 06:03:40 -0300

    Carregando os dados do contato na página de edição

--- mycontacts/fe/src/pages/EditContact/index.js
+++ mycontacts/fe/src/pages/EditContact/index.js
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+import { Loader } from '../../components/Loader'
 import { ContactForm } from '../../components/ContactForm'
 import { PageHeader } from '../../components/PageHeader'
+import ContactsService from '../../services/ContactsService'
+import { toast } from '../../utils'
 
 export function EditContact() {
+  const [isLoading, setIsLoading] = useState(true)
+  const { id } = useParams()
+  const history = useHistory()
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id)
+
+        console.log({ contactData })
+        setIsLoading(false)
+      } catch (error) {
+        history.push('/')
+        toast({ type: 'danger', text: 'Contato não encontrado!' })
+      }
+    }
+
+    loadContact()
+  }, [id, history])
+
+  function handleSubmit() {}
+
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar Mike Livramento" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
     </>
   )
 }
