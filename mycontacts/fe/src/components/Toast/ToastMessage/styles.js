731de9cbe8fb33e4f8db0e7f92c1d9edf8d1db86
Alessandre Livramento <aleliv74@gmail.com>
2023-02-06 16:11:47 -0300

    Animando a entrada de components em listas

--- mycontacts/fe/src/components/Toast/ToastMessage/styles.js
+++ mycontacts/fe/src/components/Toast/ToastMessage/styles.js
@@ -1,4 +1,17 @@
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+const messageIn = keyframes`
+  from {
+    opacity: 0 ;
+    transform: translateY(100px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0px)
+  }
+  
+`
 
 const containerVariants = {
   default: css`
@@ -22,6 +35,7 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     cursor: pointer;
+    animation: ${messageIn} 0.3s;
 
     &:focus {
       background: purple;
