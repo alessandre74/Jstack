eb8e513274e782b7beb4570ef9e4bc5082034f4c
Alessandre Livramento <aleliv74@gmail.com>
2023-09-03 05:30:57 -0300

    Curso JStack

--- projects/fincheck/api/src/app.module.ts
+++ projects/fincheck/api/src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
   controllers: [],
   providers: [
     {
