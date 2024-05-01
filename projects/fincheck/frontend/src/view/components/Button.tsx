0924aff16f5f063fde29fe6396ae724eda4efc6d
Alessandre Livramento <aleliv74@gmail.com>
2024-05-01 01:24:47 -0300

    Curso JStack

--- projects/fincheck/frontend/src/view/components/Button.tsx
+++ projects/fincheck/frontend/src/view/components/Button.tsx
@@ -4,16 +4,25 @@ import { Spinner } from './Spinner'
 
 type ButtonProps = ComponentProps<'button'> & {
   isLoading?: boolean
+  danger?: boolean
 }
 
-export function Button({ className, isLoading, disabled, children, ...props }: ButtonProps) {
+export function Button({
+  className,
+  isLoading,
+  disabled,
+  children,
+  danger,
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
       disabled={disabled || isLoading}
       className={cn(
         'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl  font-medium text-white transition-all flex justify-center items-center',
-        className
+        danger && 'bg-red-900 hover:bg-red-800',
+        className,
       )}
     >
       {!isLoading && children}
--- projects/fincheck/frontend/src/view/components/ConfirmDeleteModal.tsx
+++ projects/fincheck/frontend/src/view/components/ConfirmDeleteModal.tsx
@@ -1,4 +1,6 @@
+import { Button } from './Button'
 import { Modal } from './Modal'
+import { TrashIcon } from './icons/TrashIcon'
 
 interface ConfirmDeleteModalProps {
   onClose(): void
@@ -7,7 +9,25 @@ interface ConfirmDeleteModalProps {
 export function ConfirmDeleteModal({ onClose }: ConfirmDeleteModalProps) {
   return (
     <Modal open title="Excluir" onClose={onClose}>
-      Conteudo...
+      <div className="flex flex-col items-center text-center gap-6">
+        <div className="w-[52px] h-[52px] rounded-full bg-red-0 flex items-center justify-center">
+          <TrashIcon className="w-6 h-6 text-red-900" />
+        </div>
+        <p className="w-[180px] text-gray-800 tracking-[-0.5px] font-bold">
+          Tem certeza que deseja excluir está conta?
+        </p>
+        <p className="tracking-[-0.5px] text-gray-800">
+          Ao excluir a conta, também serão excluídos todos os registros de receita e despesas
+          relacionados.
+        </p>
+      </div>
+
+      <div className="mt-10 space-y-4">
+        <Button className="w-full" danger>
+          Sim, desejo excluir
+        </Button>
+        <Button className="w-full">Cancelar</Button>
+      </div>
     </Modal>
   )
 }
